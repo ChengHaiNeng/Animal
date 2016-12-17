974a4980a785dd6dfa0436934a4e989a7477bd54
laojian <792318737@qq.com>
2016-12-17 10:14:22 +0800

    Modify some database defaults bug

--- Public/animal.sql
+++ Public/animal.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `animal`.`user` (
   `regtime` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '用户注册时间 ',
   `role` TINYINT NOT NULL DEFAULT 0 COMMENT '用户角色区分,0代表普通用户,1代表认证用户',
   `salt` VARCHAR(45) NOT NULL DEFAULT '' COMMENT '密码盐',
-  `lastime` INT NOT NULL DEFAULT '' COMMENT '最近一次登录时间',
+  `lastime` INT NOT NULL DEFAULT 0 COMMENT '最近一次登录时间',
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC),
@@ -67,8 +67,8 @@ CREATE TABLE IF NOT EXISTS `animal`.`comment` (
   `content` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '用户评论内容',
   `parentid` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '上一级评论id',
   `time` INT NOT NULL DEFAULT 0,
-  `fromid` INT UNSIGNED NOT NULL DEFAULT '',
-  `toid` INT UNSIGNED NOT NULL DEFAULT '',
+  `fromid` INT UNSIGNED NOT NULL DEFAULT 0,
+  `toid` INT UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`))
 ENGINE = Myisam;
 
@@ -78,11 +78,11 @@ ENGINE = Myisam;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `animal`.`article` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `content` TEXT NOT NULL DEFAULT '' COMMENT '发布文章内容',
+  `content` TEXT NOT NULL COMMENT '发布文章内容',
   `title` VARCHAR(45) NOT NULL DEFAULT '' COMMENT '发布文章主题',
-  `pubtime` INT NOT NULL DEFAULT '' COMMENT '发布文章时间',
+  `pubtime` INT NOT NULL DEFAULT 0 COMMENT '发布文章时间',
   `click_count` INT NOT NULL DEFAULT 0 COMMENT '用户浏览量',
-  `uid` INT UNSIGNED NOT NULL DEFAULT '' COMMENT '发表文章那个人的id',
+  `uid` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '发表文章那个人的id',
   PRIMARY KEY (`id`),
   INDEX `fk_article_user1_idx` (`uid` ASC),
   CONSTRAINT `fk_article_user1`
@@ -98,8 +98,8 @@ ENGINE = Myisam;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `animal`.`zan` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `aid` INT UNSIGNED NOT NULL DEFAULT '' COMMENT '被点赞的文章的id',
-  `uid` INT NOT NULL DEFAULT '' COMMENT '用户点赞的id',
+  `aid` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '被点赞的文章的id',
+  `uid` INT NOT NULL DEFAULT 0 COMMENT '用户点赞的id',
   `time` INT NULL DEFAULT 0 COMMENT '点赞时间',
   PRIMARY KEY (`id`))
 ENGINE = Myisam;
@@ -110,7 +110,7 @@ ENGINE = Myisam;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `animal`.`pic` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `uid` INT NOT NULL DEFAULT '' COMMENT '用户id',
+  `uid` INT NOT NULL DEFAULT 0 COMMENT '用户id',
   `aid` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '文章id,如果为0,就不存在 任何文章当中',
   `upload` VARCHAR(45) NULL DEFAULT '' COMMENT '上传服务器图片地址',
   `thumb` VARCHAR(45) NULL DEFAULT '' COMMENT '所有缩略图的地址',
@@ -138,7 +138,7 @@ ENGINE = Myisam;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `animal`.`video` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `uid` INT NOT NULL DEFAULT '' COMMENT '用户id',
+  `uid` INT NOT NULL DEFAULT 0 COMMENT '用户id',
   `aid` INT UNSIGNED NOT NULL DEFAULT 0 COMMENT '文章id,如果为0,就不存在 任何文章当中',
   `upload` VARCHAR(45) NULL DEFAULT '' COMMENT '上传服务器图片地址',
   `name` VARCHAR(45) NULL DEFAULT '' COMMENT '照片昵称',
